53f985791e97091d8fa852302569385535015aa1
pehjos <71464455+pehjos@users.noreply.github.com>
2021-11-04 04:39:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use((req,res, next)=>{
  
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://pehs.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
   res.setHeader('Access-Control-Allow-Origin',"*");
