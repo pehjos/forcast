894d30027030fd511b26d49afadea6dd0c003245
pehjos <71464455+pehjos@users.noreply.github.com>
2021-11-04 04:36:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,20 @@ import userRouter from "./routes/user.js";
 
 
 const app = express();
-
+app.use(cors());
+app.use((req,res, next)=>{
+ 
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+  res.setHeader('Access-Control-Allow-Origin',"*");
+  res.setHeader('Access-Control-Allow-Headers',"*");
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
